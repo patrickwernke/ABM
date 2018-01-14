5ff7f4c9bada29fc1776f22d202d64a725554316
Martijn Dortmond <martijndortmond@hotmail.com>
2018-01-14 12:47:39 +0100

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def plot_ducks(plot_m, plot_f, model, pause=0.01):
     x_f = []
     y_f = []
     for agent in model.schedule.agents:
-        if isinstance(agent, FemaleDuckAgent):        
+        if isinstance(agent, FemaleDuckAgent):
             x_f.append(agent.pos[0])
             y_f.append(agent.pos[1])
         else:
@@ -18,25 +18,21 @@ def plot_ducks(plot_m, plot_f, model, pause=0.01):
     plot_m.set_xdata(x_m)
     plot_f.set_ydata(y_f)
     plot_f.set_xdata(x_f)
-    
+
     plt.draw()
     plt.pause(pause)
 
-
-
-
-
 if __name__ == '__main__':
     model = DuckModel(10, 10, 100, 100)
-    
+
     plt.ion()
     fig=plt.figure()
     ax=fig.add_subplot(111)
-    
+
     ax.set_xlim(0,100)
     ax.set_ylim(0,100)
-    plot_m, = ax.plot([], [], 'o', color='xkcd:blue')
-    plot_f, = ax.plot([], [], 'o', color='xkcd:hot pink')
+    plot_m, = ax.plot([], [], 'o', color='b')
+    plot_f, = ax.plot([], [], 'o', color='r')
     plt.show()
     plot_ducks(plot_m, plot_f, model, 0.05)
     for i in range(200):
