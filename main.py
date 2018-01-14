01de7a72f6ab982a7f285b041efa77adcbaf8ff9
Patrick Wernke <patrick@bit-students.com>
2018-01-14 13:04:42 +0100

    added working pause button

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ class DuckVisualisation():
     def __init__(self, Model):
         self.Model = Model
         self.sliders = {}
+        self.t = 0
         self.tickrate = 0.01
+        self.halt = True
         plt.ion()
 
         # give the window a fixed size
@@ -27,22 +29,40 @@ class DuckVisualisation():
         plt.show()
 
         # create a slider for the tickrate
-        tick_axes = plt.axes([0.25, 0.1, 0.65, 0.03]) #the position of the slider
-        tick_slider = Slider(tick_axes, 'Tickrate', 0.0001, 0.1, valinit=self.tickrate)
+        tick_pos = plt.axes([0.25, 0.1, 0.65, 0.03]) #the position of the slider
+        tick_slider = Slider(tick_pos, 'Tickrate', 0.0, 0.1, valinit=self.tickrate)
         tick_slider.on_changed(self.tick_slider_update)
         self.sliders['Tickrate'] = tick_slider
 
-        self.run(plot_m, plot_f)
+        # add pause/reset button
+        pause_pos = plt.axes([0.8, 0.025, 0.1, 0.04])
+        pause_button = Button(pause_pos, 'Pause/Unpause', color='r')
+        pause_button.on_clicked(self.pause_unpause)
+
+        # do a single step to show the model at start
+        self.step(plot_m, plot_f)
+
+        # keep the window open for eternity
+        while True:
+            self.run(plot_m, plot_f)
+            # run terminated, halt it again
+            self.halt = True
+            # wait a bit to run again
+            plt.pause(1)
 
     # plots the ducks (male/female) movement at a given tickrate (speed)
-    def run(self, plot_m, plot_f, max_iter=5000):
-        for i in range(max_iter):
+    def run(self, plot_m, plot_f, t_max=5000):
+        while self.t < t_max and not self.halt:
             # take a step in the model and in the visualisation
             self.Model.step()
             self.step(plot_m, plot_f)
 
             # wait for set tickrate
             plt.pause(self.tickrate)
+
+            # increment the time
+            self.t += 1
+
 
     # do 1 simulation step
     def step(self, plot_m, plot_f):
@@ -68,6 +88,11 @@ class DuckVisualisation():
     # take the new slider value
     def tick_slider_update(self, val):
         self.tickrate = val
+
+    # pause if simulation is running,
+    # unpause if simulation is halted
+    def pause_unpause(self, event):
+        self.halt = False if self.halt else True
 
 
 if __name__ == '__main__':
