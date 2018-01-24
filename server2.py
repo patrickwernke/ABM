9ee236baec0a58003d83ec5026ba0b931ff52add
Heleen Willard <helendotcom@re-stud-146-50-245-221.wireless.uva.nl>
2018-01-24 12:49:14 +0100

    eggs per nest

---
+++ server2.py
@@ -0,0 +1,43 @@
+# Adapted from the tutorial
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from duckmodel import DuckModel, FemaleDuckAgent
+from HistModule import HistogramModule
+from mesa.visualization.modules import ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
+
+WIDTH = 50
+HEIGHT = 50
+
+def duck_portrayal(duck):
+    if isinstance(duck, FemaleDuckAgent):
+        c = 'red'
+        r = 1
+        layer=0
+    else:
+        c = 'blue'
+        r = 0.5
+        layer=1
+
+    attributes = {'Shape': 'circle',
+                  'Filled': 'true',
+                  'Layer': layer,
+                  'Color': c,
+                  'r': r}
+    return attributes
+
+# Sliders
+number_agent = UserSettableParameter("slider", "Number of pairs", 20, 1, 100, 1)
+season_length = UserSettableParameter("slider", "length of season", 20, 10, 50, 1)
+mutation = UserSettableParameter("slider", "Mutation chance", 0.1, 0, 1, 0.05)
+partner_egg = UserSettableParameter("slider", "# partner mating", 20, 10, 50, 1)
+base_succes_mate = UserSettableParameter("slider", "Base succes chance of mating", 0.25, 0, 1, 0.05)
+eggs_per_nest = UserSettableParameter("slider", "Eggs per nest", 1, 0, 10, 1)
+# Graphs
+histogram = HistogramModule(list(range(1,21)), 200, 500)
+grid = CanvasGrid(duck_portrayal, WIDTH, HEIGHT, 500, 500)
+
+model_args = {'N':number_agent, 'width':WIDTH, 'height':HEIGHT, "season_length": season_length,
+                "mutation": mutation, "partner_egg": partner_egg, "base_succes_mate": base_succes_mate,
+                "eggs_per_nest": eggs_per_nest}
+server = ModularServer(DuckModel, [grid, histogram], 'Mating of ducks', model_args)
