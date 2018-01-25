cb8c76cb61afd1504c048c32c7b45e0ec8b96d89
Patrick Wernke <patrick@bit-students.com>
2018-01-25 12:50:05 +0100

    1080 iteraties aan results

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from DataExtraction import get_standard_deviations
 from numpy import mean
 import numpy as np
 from tqdm import tqdm
+import pickle
 
 def run_duck_get_stds(values, steps):
     result = np.zeros(values.shape[0])
@@ -18,10 +19,23 @@ def run_duck_get_stds(values, steps):
         result[i] = std
     return result
     
-if __name__ == '__main__':
+# get results of our model for number of iterations and write to data/name
+def make_results(name):
     problem = read_param_file('./params.txt')
-    param_vals = saltelli.sample(problem, 1, calc_second_order=True)
+    param_vals = saltelli.sample(problem, 90, calc_second_order=True)
     print("Total amount of iterations:", param_vals.shape[0])
     results = run_duck_get_stds(param_vals, 1200)
     Si = sobol.analyze(problem, results, calc_second_order=True, conf_level=0.95, print_to_console=True)
     print(Si)
+    pickle.dump( Si, open("data/" + name, "wb" ) )
+
+# get results from data/name file and analyse them
+def analyze_results(name):
+    Si = pickle.load( open("data/" + name, "rb" ) )
+    print(Si)
+    # TODO analysis
+
+
+if __name__ == '__main__':
+    # make_results('naampje_voor_bestandje')
+    analyze_results('Si_aggMating_1080it')
