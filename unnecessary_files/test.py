ec88e4c0e33cb1b3d20fc2bc5ebd5b495e8f3e82
Patrick Wernke <patrick@bit-students.com>
2018-02-07 09:45:35 +0100

    removed unnecessary files

--- unnecessary_files/test.py
+++
@@ -1,9 +0,0 @@
-from model import DuckModel, FemaleDuckAgent, MaleDuckAgent
-import matplotlib.pyplot as plt
-from pycx_gui import GUI
-
-if __name__ == '__main__':
-    models = DuckModel(10, 10, 100, 100)
-
-    cx = GUI(models)
-    cx.start()
